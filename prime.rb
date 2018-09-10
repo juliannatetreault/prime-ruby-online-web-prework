d5db71f3de935f4397d5894cb02904af25c7e78c
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-10 08:27:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,4 @@
 # Add  code here!
 def prime?(i)
-  if i < 2
-    return false
-  else array = (2..i).to_a
-    array.each do |int|
-      if (i % int == 0)
-        return false
-end
-end
-end
-true
+  
 end
